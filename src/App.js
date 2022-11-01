ee9b195338d12e06023e3b024ba1b31dab4b3ea4
Setulal Chowhan <setulalchowhan@gmail.com>
2022-11-01 19:58:42 +0600

    React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Container } from '@mui/system';
+import { Route, Routes } from 'react-router-dom';
+import './App.css'
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from './components/MainNav';
+
+
+import Trending from './pages/Trending/Trending';
+import Movies from './pages/Movies/Movies';
+import Search from './pages/Search/Search';
+import Series from './pages/Series/Series'
+import Details from './pages/Details/Details';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header/>
+    <div className="app">
+      <Container> 
+        <Routes>
+          <Route exact path='/'  element={<Trending/>}/>
+          <Route path='/movies'  element={<Movies/>}/>
+          <Route path='/series'  element={<Series/>}/>
+          <Route path='/search'  element={<Search/>}/>
+          <Route path='/:media/:id'  element={<Details/>}/>
+           
+        </Routes>
+      </Container>
+      
+      <SimpleBottomNavigation/>
+      
     </div>
+    </>
   );
 }
 
